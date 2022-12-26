cedda5d7654996de1c7fe1dc6638dd03c613d1c9
ivailochav <ivailochav@gmail.com>
2022-12-26 13:17:00 +0200

    Add project files.

---
+++ ArtVenue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtVenue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
