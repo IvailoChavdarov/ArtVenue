d64b3b4d903ddc5f23b0b58bd46ca119b2914e76
ivailochav <ivailochav@gmail.com>
2023-01-10 21:07:55 +0200

    Added loader functionality and design

---
+++ ArtVenue/wwwroot/js/loader.js
@@ -0,0 +1,12 @@
+﻿$("a").not(".NoLoader").not(".close").click(function () {
+    showLoader()
+});
+$("button").not(".NoLoader").not(".close").click(function () {
+    showLoader()
+});
+function hideLoader() {
+    document.getElementById("loaderContainer").style.display = "none"
+}
+function showLoader() {
+    document.getElementById("loaderContainer").style.display = "block"
+}
