66fd834673bb7a14ee9d4f3e09c5800b6a4b5661
ivailochav <ivailochav@gmail.com>
2023-01-12 22:17:32 +0200

    Added page listing technologies used in the creation of the website

--- ArtVenue/Controllers/HomeController.cs
+++ ArtVenue/Controllers/HomeController.cs
@@ -22,6 +22,14 @@ namespace ArtVenue.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Technologies()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
